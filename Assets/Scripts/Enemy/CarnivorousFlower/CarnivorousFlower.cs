1f304e3293488cae0add81b585efae6593821eb5
Mạnh <manh23092004@gmail.com>
2024-04-05 21:45:53 +0700

    Flower
    ok

--- Assets/Scripts/Enemy/CarnivorousFlower/CarnivorousFlower.cs
+++ Assets/Scripts/Enemy/CarnivorousFlower/CarnivorousFlower.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarnivorousFlower : MonoBehaviour
+public class CarnivorousFlower : Enemy
 {
     // Start is called before the first frame update
-    void Start()
+    public CarnivorousFlowerIdleState idleState { get; private set; }
+    public CarnivorousFlowerAttackState attackState { get; private set; }
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+
+        idleState = new CarnivorousFlowerIdleState(this, stateMachine, "Idle", this);
+        attackState = new CarnivorousFlowerAttackState(this, stateMachine, "Attack");
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Start()
     {
-        
+        base.Start();
+        stateMachine.Initialize(attackState);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
     }
 }
--- Assets/Scripts/Enemy/CarnivorousFlower/CarnivorousFlowerGroundState.cs
+++ Assets/Scripts/Enemy/CarnivorousFlower/CarnivorousFlowerGroundState.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarnivorousFlowerGroundState : MonoBehaviour
+public class CarnivorousFlowerGroundState : EnemyState
 {
-    // Start is called before the first frame update
-    void Start()
+    protected CarnivorousFlower CarnivorousFlower;
+    public CarnivorousFlowerGroundState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBollName, CarnivorousFlower carnivorousFlower) : base(_enemyBase, _stateMachine, _animBollName)
     {
-        
+        this.CarnivorousFlower = carnivorousFlower;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Enter()
     {
-        
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
     }
 }
