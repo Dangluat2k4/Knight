86d4fa33d704a0b294dd2f728d2bb6162bf95c0b
VinhTienDo <ledoanvinh96@gmail.com>
2024-04-07 12:05:37 +0700

    PupleWitch  playerDetected

--- Assets/Scripts/Enemy/PurpleWitch/PurpleWitchAttackState.cs
+++ Assets/Scripts/Enemy/PurpleWitch/PurpleWitchAttackState.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PurpleWitchAttackState : EnemyState
 {
-    public PurpleWitchAttackState(PurpleWitch _purpleWitch, EnemyStateMachine _stateMachine, string _animBoolName) : base(_purpleWitch, _stateMachine, _animBoolName)
+    private PurpleWitch purpleWitch;
+    public PurpleWitchAttackState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBollName, PurpleWitch _purpleWitch) : base(_enemyBase, _stateMachine, _animBollName)
     {
+        this.purpleWitch = _purpleWitch;
     }
 
     public override void Enter()
@@ -16,10 +18,16 @@ public class PurpleWitchAttackState : EnemyState
     public override void Exit()
     {
         base.Exit();
+        purpleWitch.lastTimeAttacked = Time.time;
     }
 
     public override void Update()
     {
         base.Update();
+        purpleWitch.SetZeroVelocity();
+        if (triggerCalled)
+        {
+            stateMachine.ChangeState(purpleWitch.battleState);
+        }
     }
 }
--- Assets/Scripts/Enemy/PurpleWitch/PurpleWitchMoveState.cs
+++ Assets/Scripts/Enemy/PurpleWitch/PurpleWitchMoveState.cs
@@ -41,6 +41,10 @@ public class PurpleWitchMoveState : EnemyState
 
 
         }
+        else if(purple.IsPlayerDetected())
+        {
+            stateMachine.ChangeState(purple.battleState);
+        }
 
 
         // muốn chạy code thì phải update lên 
